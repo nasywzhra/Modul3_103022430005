fc2555182d9904a156a16fe142dd6301908a19ef
Nasywa <nasywazahranailah@gmail.com>
2026-03-10 08:13:46 +0700

    Menambahkan rumus celcius, fahreinheit dan kelvin

--- Modul3_103022430005/Form1.Designer.cs
+++ Modul3_103022430005/Form1.Designer.cs
@@ -28,12 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label1 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Celcius", "Farenheit", "Kelvin", "Reamur" });
+            comboBox1.Location = new Point(40, 160);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Celcius", "Fahreinheit", "Kelvin", "Reamur" });
+            comboBox2.Location = new Point(40, 269);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(182, 33);
+            comboBox2.TabIndex = 1;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 119);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Satuan Awal";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(605, 162);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 3;
+            button1.Text = "Convert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(255, 162);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(313, 31);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(255, 119);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 25);
+            label2.TabIndex = 5;
+            label2.Text = "Nilai Awal";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(259, 272);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(313, 31);
+            textBox2.TabIndex = 6;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(259, 232);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 25);
+            label3.TabIndex = 7;
+            label3.Text = "Nilai Akhir";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(44, 232);
+            label4.Name = "label4";
+            label4.Size = new Size(112, 25);
+            label4.TabIndex = 8;
+            label4.Text = "Satuan Akhir";
+            label4.Click += label4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
     }
 }
--- Modul3_103022430005/Form1.cs
+++ Modul3_103022430005/Form1.cs
@@ -6,5 +6,80 @@ namespace Modul3_103022430005
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1 || comboBox2.SelectedIndex == -1)
+            {
+                MessageBox.Show("Pilih satuan terlebih dahulu!");
+                return;
+            }
+
+            double nilaiAwal;
+            bool isAngkaValid =
+                double.TryParse(textBox1.Text, out nilaiAwal);
+            if (!isAngkaValid)
+            {
+                MessageBox.Show("Masukkan angka yang valid!");
+                return;
+            }
+
+            string from = comboBox1.SelectedItem.ToString();
+            string to = comboBox2.SelectedItem.ToString();
+            double hasil = 0;
+
+            if (from == "Celcius" && to == "Fahreinheit")
+            {
+                hasil = (9 / 5) * nilaiAwal + 32;
+                textBox2.Text = hasil.ToString();
+            }
+            else if (from == "Celcius" && to == "Reamur")
+            {
+                hasil = (4.0 / 5.0) * nilaiAwal;
+            }
+            else if (from == "Celcius" && to == "Kelvin")
+            {
+                hasil = nilaiAwal + 273.15;
+            }
+        }
     }
 }
